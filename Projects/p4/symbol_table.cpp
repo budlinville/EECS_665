6c478860950ee454348687ed68c9659c6e0011dc
westonhack <westonhack18@gmail.com>
2018-11-05 19:15:32 -0600

    Add files via upload

--- Projects/p4/symbol_table.cpp
+++ Projects/p4/symbol_table.cpp
@@ -16,6 +16,14 @@ bool ScopeTable::isInScope(std::string id){
 	return false;
 }
 
+void ScopeTable::addEntry(SymbolTableEntry* e) {
+	map->insert({e->getKind(), e});
+}
+
+SymbolTableEntry * ScopeTable::getEntry(std::string id) {
+	return map->at(id);
+}
+
 /*****************************************
 		Symbol Table
 *****************************************/
@@ -23,6 +31,7 @@ SymbolTable::SymbolTable(){
 	//TODO: implement the list of hashtables
 	// approach to building a symbol table
 	scopeTables = new std::list<ScopeTable *>();
+	enterNewScope();
 };
 
 void SymbolTable::enterNewScope() {
@@ -31,7 +40,13 @@ void SymbolTable::enterNewScope() {
 }
 
 void SymbolTable::leaveCurrentScope() {
-	scopeTables->pop_front();
+	if (scopeTables->size() <= 1) {
+
+	}
+	else {
+			scopeTables->pop_front();
+	}
+
 }
 
 bool SymbolTable::isInScope(std::string id){
@@ -43,11 +58,51 @@ bool SymbolTable::isInScope(std::string id){
 	return false;
 }
 
+void SymbolTable::addEntry(SymbolTableEntry* e) {
+	scopeTables->front()->addEntry(e);
+}
+
+SymbolTableEntry * SymbolTable::getEntry(std::string id) {
+	for (std::list<ScopeTable *>::iterator it = scopeTables->begin(); it != scopeTables->end(); ++it) {
+		ScopeTable * current = *it;
+		if (current->isInScope(id)) {
+			return current->getEntry(id);
+		}
+	}
+	return nullptr;
+}
+
 int SymbolTable::getHighestLevel() {
 	if (scopeTables->front() == nullptr) {
 		return 0;
 	}
 	return scopeTables->front()->level;
+}
+
+std::string FnSymbolEntry::getType() {
+	std::string type = "";
+	for (std::list<VarSymbolEntry *>::iterator it = myArgs->begin(); it != myArgs->end(); ++it) {
+		VarSymbolEntry * e = *it;
+		type += e->getType();
+
+		if (next(it) != myArgs->end()) {
+			type += ",";
+		}
+	}
+
+	type += "->" + myType;
+	return type;
+}
+
+
+VarSymbolEntry * StructSymbolEntry::find(std::string id) {
+	for (std::list<VarSymbolEntry *>::iterator it = myProps->begin(); it != myProps->end(); ++it) {
+		VarSymbolEntry * e = *it;
+		if (e->getKind() == id) {
+			return e;
+		}
+	}
+	return nullptr;
 }
 
 }	//namespace LILC
--- Projects/p4/symbol_table.hpp
+++ Projects/p4/symbol_table.hpp
@@ -8,9 +8,11 @@ namespace LILC{
 //A single entry for one name in the symbol table
 class SymbolTableEntry{
 public:
-	std::string type;
-	std::string kind;	//NOTE: maybe AstNode
+	virtual std::string getType() = 0;
+	virtual std::string getKind() = 0;
 };
+
+
 
 //A single
 class ScopeTable{
@@ -21,6 +23,8 @@ class ScopeTable{
 		// that the symbol does not exist within
 		// the current scope
 		bool isInScope(std::string id);
+		SymbolTableEntry * getEntry(std::string id);
+		void addEntry(SymbolTableEntry * e);
 		int level;
 
 
@@ -35,10 +39,79 @@ class SymbolTable{
 		void enterNewScope();
 		void leaveCurrentScope();
 		bool isInScope(std::string id);
+		void addEntry(SymbolTableEntry * e);
+		SymbolTableEntry * getEntry(std::string id);
 
 	private:
 		std::list<ScopeTable *> * scopeTables;
 		int getHighestLevel();
+};
+
+class VarSymbolEntry : public SymbolTableEntry {
+public:
+	VarSymbolEntry(std::string type, std::string kind, int size) {
+		myType = type;
+		myKind = kind;
+		mySize = size;
+	}
+	int getSize() {
+		return mySize;
+	}
+	std::string getKind() {
+		return myKind;
+	}
+	std::string getType() {
+		return myType;
+	}
+
+private:
+	std::string myKind;
+	std::string myType;
+	int mySize;
+};
+
+
+class FnSymbolEntry : public SymbolTableEntry {
+public:
+	FnSymbolEntry(std::string type, std::string kind, std::list<VarSymbolEntry *> * vars) {
+		myType = type;
+		myKind = kind;
+		myArgs = vars;
+	}
+
+	std::string getKind() {
+		return myKind;
+	}
+
+	std::string getType();
+
+private:
+	std::string myKind;
+	std::list<VarSymbolEntry *> * myArgs;
+	std::string myType;
+};
+
+
+class StructSymbolEntry : public SymbolTableEntry {
+public:
+	StructSymbolEntry(std::string kind, std::list<VarSymbolEntry *> * props) {
+		myKind = kind;
+		myProps = props;
+	}
+	std::string getKind() {
+		return myKind;
+	}
+
+	std::string getType() {
+			return myKind;
+	}
+
+
+	VarSymbolEntry * find(std::string id);
+
+private:
+	std::string myKind;
+	std::list<VarSymbolEntry *> * myProps;
 };
 
 }
